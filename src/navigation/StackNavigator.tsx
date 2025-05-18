6e8cd9676b03cd45fb1ddaa1e022abb87937bdd3
Victormoroo <victorhugo0609@outlook.com>
2025-05-18 16:48:24 -0300

    feat: criacao das subpastas e inciado a Navigation do projeto

---
+++ src/navigation/StackNavigator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function StackNavigator() {
+  return (
+  );
+}
