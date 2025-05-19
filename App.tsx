436b9d514a005c5215b2b9a28cf6989ca71cb36f
Victormoroo <victorhugo0609@outlook.com>
2025-05-19 02:19:01 -0300

    feat: rotas alteradas usando native-base, e link com api pausada para finalizacao do projeto

--- App.tsx
+++ App.tsx
@@ -1,5 +1,11 @@
+import React from 'react';
+import { NativeBaseProvider } from 'native-base';
 import StackNavigator from './src/navigation/StackNavigator';
 
 export default function App() {
-  return <StackNavigator />;
+  return (
+    <NativeBaseProvider>
+      <StackNavigator />
+    </NativeBaseProvider>
+  );
 }
